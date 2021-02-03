6416c5502521b272f82408ec62545c361b5058cc
Lucas Gomes Santiago <lucasgomessantiago@gmail.com>
2021-02-03 20:22:02 -0300

    criação do Dockerfile e docker-compose, e anotação do modelo como entidade de banco de dados

--- src/main/java/com/bp/customerapi/domain/Customer.java
+++ src/main/java/com/bp/customerapi/domain/Customer.java
@@ -2,21 +2,47 @@ package com.bp.customerapi.domain;
 
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
-import java.util.Date;
+import java.time.LocalDateTime;
 
+@Document("customer")
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class Customer implements Serializable {
 
+    @Id
+    @Column(length = 36)
     private String id;
+
+    @NotNull
+    @Column(length = 60)
     private String name;
+
+    @NotNull
+    @Column(length = 11, unique = true)
     private String cpf;
+
+    @NotNull
+    @Column(length = 254)
     private String address;
-    private Date createDate;
-    private Date updateDate;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updateDate;
 
     public Customer(String id, String name, String cpf, String address) {
         this.id = id;
--- src/main/java/com/bp/customerapi/infrastructure/mappers/CustomerMapper.java
+++ src/main/java/com/bp/customerapi/infrastructure/mappers/CustomerMapper.java
@@ -1,6 +1,7 @@
 package com.bp.customerapi.infrastructure.mappers;
 
 import com.bp.customerapi.application.commands.CreateCustomerCommand;
+import com.bp.customerapi.application.commands.UpdateCustomerCommand;
 import com.bp.customerapi.application.queries.results.CustomerResult;
 import com.bp.customerapi.domain.Customer;
 import org.mapstruct.IterableMapping;
@@ -14,6 +15,7 @@ public interface CustomerMapper {
     CustomerMapper INSTANCE = Mappers.getMapper(CustomerMapper.class);
 
     Customer toDomain(CreateCustomerCommand command);
+    Customer toDomain(UpdateCustomerCommand command);
     CustomerResult toResult(Customer source);
     @IterableMapping(elementTargetType = CustomerResult.class)
     List<CustomerResult> toResult(List<Customer> customers);
