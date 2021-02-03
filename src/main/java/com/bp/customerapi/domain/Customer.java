39f277eb5da8e2d14c5c0ecf04115d13ad0477fd
Lucas Gomes Santiago <lucasgomessantiago@gmail.com>
2021-02-03 17:49:42 -0300

    domain model implementation

---
+++ src/main/java/com/bp/customerapi/domain/Customer.java
@@ -0,0 +1,27 @@
+package com.bp.customerapi.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer implements Serializable {
+
+    private String id;
+    private String name;
+    private String cpf;
+    private String address;
+    private Date createDate;
+    private Date updateDate;
+
+    public Customer(String id, String name, String cpf, String address) {
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.address = address;
+    }
+}
